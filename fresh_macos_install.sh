e65d3e4716400cf640c4f1278f7e0caebfd802d5
Robert G <rob@anicyber.com>
2022-09-28 15:13:23 -0400

    Update fresh_macos_install.sh

--- fresh_macos_install.sh
+++ fresh_macos_install.sh
@@ -1,1 +1,6 @@
-defaults write com.apple.dock persistent-apps -array
+# Pretty Dock 
+defaults delete com.apple.dock persistent-apps
+defaults delete com.apple.dock persistent-others
+defaults write com.apple.dock largesize -int 75
+defaults write com.apple.dock tilesize -int 50
+defaults write com.apple.dock magnification -bool true
