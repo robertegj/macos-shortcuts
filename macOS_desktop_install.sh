44de013ce5650ab7ca51bba69a71caa3c8788555
Robert G <rob@anicyber.com>
2022-09-28 15:20:19 -0400

    Update macOS_desktop_install.sh

--- macOS_desktop_install.sh
+++ macOS_desktop_install.sh
@@ -2,7 +2,7 @@
 xcode-select --install
 
 # Install Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Reset brew
 brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
