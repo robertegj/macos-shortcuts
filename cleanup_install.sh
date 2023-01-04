2ac400250275e824ac9171033588abecf4a9b82e
Robert G <rob@anicyber.com>
2023-01-04 18:59:39 -0500

    Update cleanup_install.sh

--- cleanup_install.sh
+++ cleanup_install.sh
@@ -4,3 +4,4 @@ defaults delete com.apple.dock persistent-others
 defaults write com.apple.dock largesize -int 75
 defaults write com.apple.dock tilesize -int 50
 defaults write com.apple.dock magnification -bool true
+killall Dock
